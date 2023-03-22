bd545c318c966b876b737fdaeaeed244e97f937f
daiterranova <daiana.terranova@gmail.com>
2023-03-22 20:55:54 -0300

    create useEffect hook to fetch data

--- src/components/Autocomplete.jsx
+++ src/components/Autocomplete.jsx
@@ -1,12 +1,33 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import finnHub from "../apis/finnHub"
 
 export const Autocomplete = () => {
     const [search, setSearch] = useState("")
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await finnHub.get("/search", {
+                    params: {
+                        q: search
+                    }
+                })
+            }
+            catch (err) {
+
+            }
+        }
+        if (search.length > 0) {
+            fetchData()
+        }
+
+    }, [search])
+
     return (
         <div className="w-50 p-5 rounded mx-auto">
             <div className="form-floating dropdown">
-                <input style={{backgroundColor: 'rgba(145, 158, 171, 0.04)'}}
-                id="search" type="text" className="form-control" placeholder="Search" autoComplete="off" value={search} onChange={(e) => setSearch(e.target.value)}></input>
+                <input style={{ backgroundColor: 'rgba(145, 158, 171, 0.04)' }}
+                    id="search" type="text" className="form-control" placeholder="Search" autoComplete="off" value={search} onChange={(e) => setSearch(e.target.value)}></input>
                 <label htmlFor="search">Search</label>
                 <ul className="dropdown-menu">
                     <li>stock1</li>
