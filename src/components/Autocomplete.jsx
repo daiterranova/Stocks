0f71c2655681082dcdbfff8c9c0dc74bf7c7b562
daiterranova <daiana.terranova@gmail.com>
2023-03-22 20:36:13 -0300

    add basic component structure

--- src/components/Autocomplete.jsx
+++ src/components/Autocomplete.jsx
@@ -1,6 +1,19 @@
+import { useState } from "react"
 
 export const Autocomplete = () => {
+    const [search, setSearch] = useState("")
     return (
-        <div>Autocomplete component</div>
+        <div className="w-50 p-5 rounded mx-auto">
+            <div className="form-floating dropdown">
+                <input style={{backgroundColor: 'rgba(145, 158, 171, 0.04)'}}
+                id="search" type="text" className="form-control" placeholder="Search" autoComplete="off" value={search} onChange={(e) => setSearch(e.target.value)}></input>
+                <label htmlFor="search">Search</label>
+                <ul className="dropdown-menu">
+                    <li>stock1</li>
+                    <li>stock2</li>
+                    <li>stock3</li>
+                </ul>
+            </div>
+        </div>
     )
 }
