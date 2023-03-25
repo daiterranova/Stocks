1085cb48f1284f71f1d9221a5ae743d78c522a5c
daiterranova <daiana.terranova@gmail.com>
2023-03-25 20:45:42 -0300

    format code

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -1,48 +1,47 @@
 import { useState, useEffect } from "react"
 import finnHub from "../apis/finnHub"
-import { BsFillCaretUpFill, BsFillCaretDownFill} from "react-icons/bs"
+import { BsFillCaretUpFill, BsFillCaretDownFill } from "react-icons/bs"
 
 export const StockList = () => {
     const [stock, setStock] = useState([])
-    const [watchList, setWatchList] = useState(["GOOGL", "MSFT", "AMZN"])
-    
-    const changeColor = (change) =>  change > 0 ? "success" : "danger";
 
-    const renderIcon = (change) => change > 0 ? <BsFillCaretUpFill/> : <BsFillCaretDownFill/>;
+    const changeColor = (change) => change > 0 ? "success" : "danger";
+
+    const renderIcon = (change) => change > 0 ? <BsFillCaretUpFill /> : <BsFillCaretDownFill />;
 
     useEffect(() => {
         let isMounted = true;
         const fetchData = async () => {
             try {
-              const responses = await Promise.all(watchList.map((stock) => {
+                const responses = await Promise.all(watchList.map((stock) => {
                     return finnHub.get('/quote', {
-                        params:{
-                            symbol:stock
+                        params: {
+                            symbol: stock
                         }
-                    })  
+                    })
                 }))
-            console.log(responses)
-              const data =  responses.map((response)=>{
-                   return {
+                console.log(responses)
+                const data = responses.map((response) => {
+                    return {
                         data: response.data,
                         symbol: response.config.params.symbol
                     }
 
-                 })
+                })
                 console.log(data)
-                if(isMounted){
+                if (isMounted) {
                     setStock(data)
                 }
             }
-            catch(err){
+            catch (err) {
 
             }
         }
         fetchData()
         return () => (isMounted = false)
-    },[])
-    
-    
+    }, [])
+
+
     return (
         <div>
             <table className="table hover mt-5">
@@ -60,19 +59,19 @@ export const StockList = () => {
                 </thead>
                 <tbody>
                     {
-                        stock.map((stockData) =>{
-                           return (
-                            <tr className="table-row" key={stockData.symbol}>
-                                <th scope="row">{stockData.symbol}</th>
-                                <td>{stockData.data.c}</td>
-                                <td className={`text-${changeColor(stockData.data.d)}`}>{stockData.data.d}{renderIcon(stockData.data.d)}</td>
-                                <td className={`text-${changeColor(stockData.data.dp)}`}>{stockData.data.dp}{renderIcon(stockData.data.d)}</td>
-                                <td>{stockData.data.h}</td>
-                                <td>{stockData.data.l}</td>
-                                <td>{stockData.data.o}</td>
-                                <td>{stockData.data.pc}</td>
-                            </tr>
-                           )
+                        stock.map((stockData) => {
+                            return (
+                                <tr className="table-row" key={stockData.symbol}>
+                                    <th scope="row">{stockData.symbol}</th>
+                                    <td>{stockData.data.c}</td>
+                                    <td className={`text-${changeColor(stockData.data.d)}`}>{stockData.data.d}{renderIcon(stockData.data.d)}</td>
+                                    <td className={`text-${changeColor(stockData.data.dp)}`}>{stockData.data.dp}{renderIcon(stockData.data.d)}</td>
+                                    <td>{stockData.data.h}</td>
+                                    <td>{stockData.data.l}</td>
+                                    <td>{stockData.data.o}</td>
+                                    <td>{stockData.data.pc}</td>
+                                </tr>
+                            )
                         })
                     }
                 </tbody>
