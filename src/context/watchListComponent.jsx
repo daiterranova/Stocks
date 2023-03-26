f453ba4ba59399f47d0114e5487d6df83cbb9418
daiterranova <daiana.terranova@gmail.com>
2023-03-25 22:48:38 -0300

    create addStock handler and added it as a child prop

--- src/context/watchListComponent.jsx
+++ src/context/watchListComponent.jsx
@@ -1,13 +1,28 @@
 import { createContext, useState } from "react";
+import { StockList } from "../components/StockList";
 
 export const WatchListContext = createContext();
 
 export const WatchListContextProvider = ({ children }) => {
 
     const [watchList, setWatchList] = useState(["GOOGL", "MSFT", "AMZN"]);
-
+    const addStock = (stock) => {
+        if (watchList.indexOf(stock) === -1) {
+            setWatchList([...watchList, stock])
+            console.log(stock)
+        }
+        /* if (!watchList.includes(stock)) {
+            setWatchList([...watchList, stock])
+        } */
+    }
+    const deleteStock = (stock) => {
+        const filteredList = watchList.filter((el) => {
+            return el !== stock
+        })
+        setWatchList(filteredList)
+    }
     return (
-        <WatchListContext.Provider value={watchList}>
+        <WatchListContext.Provider value={{ watchList, addStock, deleteStock }}>
             {children}
         </WatchListContext.Provider>
     )
