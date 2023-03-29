7f458f0d00029711d9be46d5fcd5b365c74b2de6
daiterranova <daiana.terranova@gmail.com>
2023-03-28 22:17:44 -0300

    implement remove button for delete stock

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom"
 
 export const StockList = () => {
     const [stock, setStock] = useState([])
-    const { watchList } = useContext(WatchListContext);
+    const { watchList, deleteStock } = useContext(WatchListContext);
     const changeColor = (change) => change > 0 ? "success" : "danger";
     const navigate = useNavigate();
     const renderIcon = (change) => change > 0 ? <BsFillCaretUpFill /> : <BsFillCaretDownFill />;
@@ -73,7 +73,10 @@ export const StockList = () => {
                                     <td>{stockData.data.h}</td>
                                     <td>{stockData.data.l}</td>
                                     <td>{stockData.data.o}</td>
-                                    <td>{stockData.data.pc}</td>
+                                    <td>{stockData.data.pc} <button className="btn btn-danger btn-sm ml-3 d-inline-block delete-button" onClick={(e) => {
+                                        e.stopPropagation()
+                                        deleteStock(stockData.symbol)
+                                    }}>Remove</button></td>
                                 </tr>
                             )
                         })
