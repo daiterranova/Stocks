f97738d1541f89275d898be2b04a0eb22f2bc016
daiterranova <daiana.terranova@gmail.com>
2023-03-29 18:02:58 -0300

    render logo on overview page

--- src/pages/StockOverviewPage.jsx
+++ src/pages/StockOverviewPage.jsx
@@ -1,9 +1,13 @@
 import { StockList } from "../components/StockList"
 import { Autocomplete } from "../components/Autocomplete"
+import img from "../images/trading.jpg"
 
-export  const StockOverviewPage = () => {
-  return  <div> StockOverviewPage
-    <Autocomplete/>
-    <StockList/>
-     </div>
+export const StockOverviewPage = () => {
+  return <div>
+    <div className="text-center">
+      <img src={img} />
+    </div>
+    <Autocomplete />
+    <StockList />
+  </div>
 }
