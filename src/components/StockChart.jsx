02b8016881109ca9b78fe2324fada812a48b1f43
daiterranova <daiana.terranova@gmail.com>
2023-03-28 21:47:13 -0300

    create renderButtonSelect function for change classes when button is selected

--- src/components/StockChart.jsx
+++ src/components/StockChart.jsx
@@ -48,12 +48,21 @@ export const StockChart = ({ chartData, symbol }) => {
         name: symbol,
         data: determineTimeFormat()
     }]
+
+    const renderButtonSelect = (button) => {
+        const classes = "btn m-1 "
+        if (button === dateFormat) {
+            return classes + "btn-primary"
+        } else {
+            return classes + "btn-outline-primary"
+        }
+    }
     return <div className='mt-5 p-4 shadow-sm bg-white'>
         <Chart options={options} series={series} type="area" width="100%" />
         <div>
-            <button onClick={() => { setDateFormat("24h") }}>24 hs</button>
-            <button onClick={() => { setDateFormat("7D") }}>7 days</button>
-            <button onClick={() => { setDateFormat("1Y") }}>1 Year</button>
+            <button className={renderButtonSelect("24h")} onClick={() => { setDateFormat("24h") }}>24 hs</button>
+            <button className={renderButtonSelect("7d")} onClick={() => { setDateFormat("7D") }}>7 days</button>
+            <button className={renderButtonSelect("1y")} onClick={() => { setDateFormat("1Y") }}>1 Year</button>
         </div>
     </div >
 
