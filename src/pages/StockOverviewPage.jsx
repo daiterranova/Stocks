96c12dd8b1d875b9d54ca7c4054de32708f4d6c2
daiterranova <daiana.terranova@gmail.com>
2023-03-30 11:20:47 -0300

    set inline style for width on logo img

--- src/pages/StockOverviewPage.jsx
+++ src/pages/StockOverviewPage.jsx
@@ -5,7 +5,7 @@ import img from "../images/trading.jpg"
 export const StockOverviewPage = () => {
   return <div>
     <div className="text-center">
-      <img src={img} />
+      <img src={img} width="400" />
     </div>
     <Autocomplete />
     <StockList />
